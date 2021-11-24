868dc14a40267824b2680ee1559aafa7882b5b7a
tonlls <tonlls1999@gmail.com>
2021-11-24 16:15:43 +0100

    ended

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import Config
 import Model
 import Tools
+from PIL import Image
 
 users = []
 users += Model.Contestant.get_data()
@@ -13,6 +14,22 @@ users += Model.Assistant.get_data()
 Tools.create_dir(Config.OUT_PATH)
 Tools.empty_dir(Config.OUT_PATH)
 
+# self.card.paste(image, Card.QR_POS)
+# a4im = Image.new('RGB',
+                #  (595, 842),   # A4 at 72dpi
+                #  (255, 255, 255))  # White
+# x,y=0,0
+# i=0
 for u in users:
 	u.generate_card()
+	# a4im.paste(u.card, (x,y))
+	# i+=1
+	# x+=u.card.width
+	# if(i==2):
+		# i=0
+		# y+=u.card.height
+		# x=0
 	u.save()
+# a4im.save("cards.pdf")
+
+	# break
