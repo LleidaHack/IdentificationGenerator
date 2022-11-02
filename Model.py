a8e8f3c7a34bea2a9b034dd4ab50048d00b67f41
tonlls <tonlls1999@gmail.com>
2022-11-02 01:38:00 +0100

    positions ok i think

--- Model.py
+++ Model.py
@@ -14,10 +14,10 @@ class Card:
 	QR_POS = (1301, 172)
 	QR_SIZE = (723, 723)
 	QR_BORDER_SIZE = 0
-	TYPE_POS = (0, 155)
+	TYPE_POS = (0, 500)
 	# NAME_POS = (500, 1250)
-	NAME_POS = (140, 120)
-	NICK_POS = (1198, 870)
+	NAME_POS = (0, 330)
+	COMP_NAME_POS = (0, 200)
 	# LOGO_POS = (920, 250)
 	# LOGO_SIZE = (550, 350)
 
@@ -48,9 +48,9 @@ class Assistant(object):
 
 	def generate_card(self, rgb_back=(255, 255, 255), name=None):
 		self.card = Image.open(Config.BAK_PATH)
-		Tools.draw_text(self.card, self.type, Card.TYPE_POS, Config.TYPE_FONT, Config.WHITE_FONT_COLOR)
+		Tools.draw_text(self.card, self.type, Card.TYPE_POS, Config.TYPE_FONT, Config.FONT_COLOR)
 		if not name is None:
-			Tools.draw_text(self.card, self.name, Card.NAME_POS, Config.NAME_FONT, Config.WHITE_FONT_COLOR)
+			Tools.draw_text(self.card, self.name, Card.NAME_POS, Config.NAME_FONT, Config.FONT_COLOR, mayus=False)
 
 	@staticmethod
 	def get_data():
@@ -84,6 +84,32 @@ class Volunteer(Assistant):
 		for u in data[Volunteer.__DATA]:
 			if name is None or name == u['name']:
 				res.append(Volunteer(u['name']))
+			if Config.TEST or (name is not None and name == u['name']):
+				break
+		return res
+
+class Company(Assistant):
+	__ID = 1
+	# __LOGO_PATH = os.path.join(Config.RES_PATH, 'editions', Config.EDITION, 'images', 'logogran.png')
+	__TYPE = 'EXPOSITOR'
+	__DATA = 'companies'
+
+	def __init__(self, name, company_name):
+		super().__init__('C' + str(Company.__ID), Company.__TYPE, name)
+		Company.__ID += 1
+		self.company_name = company_name
+
+	def generate_card(self, rgb_back=(255, 255, 255)):
+		super().generate_card(rgb_back, name=self.name)
+		Tools.draw_text(self.card, self.company_name, Card.COMP_NAME_POS, Config.TYPE_FONT, Config.FONT_COLOR)
+
+	@staticmethod
+	def get_data(name=None):
+		res = []
+		data = Tools.DataFile.get_content(Company._DATA_FILE, 'JSON')
+		for u in data[Company.__DATA]:
+			if name is None or name == u['name']:
+				res.append(Company(u['name']))
 			if Config.TEST or (name is not None and name == u['name']):
 				break
 		return res
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 
 users = []
 # users += Model.Contestant.get_data()
-users += Model.Organizer.get_data()
+# users += Model.Organizer.get_data()
 # users += Model.Volunteer.get_data()
 # users += Model.Company.get_data()
 # users += Model.Guest.get_data()
@@ -14,8 +14,13 @@ users += Model.Organizer.get_data()
 
 Tools.create_dir(Config.OUT_PATH)
 Tools.empty_dir(Config.OUT_PATH)
+c=Model.Company('Laura Haro Escoi', 'Lleidahack')
+for i in range(10):
+    c.generate_card()
+    c.card.save(os.path.join(Config.OUT_PATH, 'card' + str(i) + '.png'))
+# for u in users:
+# 	u.generate_card()
+# 	u.save()
+# 	break
 
-for u in users:
-	u.generate_card()
-	u.save()
-	break
+
